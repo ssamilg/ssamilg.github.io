92fcced50f855589a52a21133d52f3b4618e77fe
ssamilg <samil-98@hotmail.com>
2022-09-18 17:30:46 +0300

    vue files added

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    
+  },
+})
+</script>
+
+<template>
+  <div>
+    hello there !
+  </div>
+</template>
