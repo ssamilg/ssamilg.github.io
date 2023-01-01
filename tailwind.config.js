73fbe0ae25e4d2bfbd5b52a6d983033d2438f8fb
ssamilg <samil-98@hotmail.com>
2023-01-02 02:43:12 +0300

    feat: add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,21 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       ssg: {                 
+  //         "primary": "#10b981",
+  //         "secondary": "#f59e0b",
+  //         "accent": "#4338ca",
+  //         "neutral": "#191D24",
+  //         "base-100": "#2A303C",
+  //         "info": "#3ABFF8",
+  //         "success": "#36D399",
+  //         "warning": "#FBBD23",
+  //         "error": "#e11d48",
+  //       },
+  //     },
+  //   ],
+  // },
 }
