ced197819045b96ba9a62dcad1efec5d4c93854f
ssamilg <samil-98@hotmail.com>
2023-01-08 02:16:22 +0300

    refactor: update component structure for common elements

---
+++ src/components/common/VTabPanelCard.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'VTabPanelCard',  
+}
+</script>
+
+<template>
+  <div class="card bg-base-200">
+    <div class="card-body text-center">
+      <slot />
+    </div>
+  </div>
+</template>
