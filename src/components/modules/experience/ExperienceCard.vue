568b6b2c9afc903bd98f3bff280748d83ec27838
ssamilg <samil-98@hotmail.com>
2025-01-05 19:25:31 +0300

    refactor: update ExperienceCard component with improved layout and enhanced experience descriptions

--- src/components/modules/experience/ExperienceCard.vue
+++ src/components/modules/experience/ExperienceCard.vue
@@ -1,10 +1,9 @@
 <script>
 import VIcon from '@/components/common/VIcon.vue';
-import VTabPanelCard from '@/components/common/VTabPanelCard.vue';
 
 export default {
   name: 'ExperienceCard',
-  components: { VIcon, VTabPanelCard },
+  components: { VIcon },
   data() {
     return {
       experiences: [
@@ -13,33 +12,32 @@ export default {
           company: 'Sensity AI',
           companyLink: 'https://sensity.ai/',
           companyLogo: 'https://pbs.twimg.com/profile_images/1288045838024007682/EKo6Gp7c_400x400.jpg',
+          // companyLogo: 'https://sensity.ai/wp-content/uploads/2024/04/sensity-logo-square.png',
           role: 'Frontend Developer',
-          description: 'I am responsible for the Sensity\'s JS based products. I am maintaining the platform project and SDK\'s, improving the code quality and adding new features.',
-          isCurrent: true,
-          technologies: ['js', 'vuejs', 'bootstrap5', 'github', 'docker', 'jest', 'sass']
+          description: 'Leading frontend development for Sensity\'s JavaScript-based products and SDK solutions. Driving platform improvements through modern development practices and architectural enhancements.',
+          technologies: ['Vue.js', 'Tailwind', 'Docker', 'GitHub CI/CD', 'Vitest']
         },
         {
           id: 1,
           company: 'Pitcher AG',
           companyLink: 'https://www.pitcher.com/',
           companyLogo: 'https://play-lh.googleusercontent.com/9RgJbmjOoVNULZKi--OUvjxUr1isDGO7LBcpGna5pY59H-FDHouKrD0MnXi9OVMOViI',
+          // companyLogo: 'https://sc-images-prod.s3.eu-central-1.amazonaws.com/logos/1c214b62-b87f-46e7-a982-44f449fe33df_logo.jpg',
           role: 'Software Engineer',
-          description: 'I was part of the Pitcher\'s product team. I worked on maintaining and renewing old applications, desinging new interfaces for cross platform (iOS, Android, Windows)',
-          isCurrent: false,
-          technologies: ['js', 'vuejs', 'vuetify', 'github', 'sass']
+          description: 'Architected cross-platform interfaces for iOS, Android, and Windows. Modernized legacy applications while ensuring consistent user experience across platforms.',
+          technologies: ['Vue.js', 'Vuetify']
         },
         {
           id: 2,
           company: 'reybex ERP',
           companyLink: 'https://www.reybex.com/en/',
-          companyLogo: 'https://play-lh.googleusercontent.com/TDS_eHqkzroSDM-96YHgx-I6h_e4QDL2AhBh-k1KQocf_h5G18x6uI3BmSOvnfgoSQ=s96-rw',
+          companyLogo: 'https://www.reybex.com/wp-content/uploads/2024/04/Reybex_Logo_Bildmarke_white_RGB-768x768.png',
+          // companyLogo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHNBjVDiCtbTCKSc4slfyAO7ID-KIXQoPAtw&s',
           role: 'Frontend Developer',
-          description: 'I was part of the team that developing reybex 2.0. I worked on maintaining existing modules and coding new interfaces for ERP modules.',
-          isCurrent: false,
-          technologies: ['js', 'vuejs', 'vuetify', 'gitlab', 'postman', 'sass']
+          description: 'Core frontend developer for reybex 2.0, building modern interfaces for complex ERP modules. Specialized in creating intuitive user experiences for enterprise software.',
+          technologies: ['Vue.js', 'Vuetify', 'Gitlab CI/CD', 'Jenkins']
         },
-      ],
-      otherTechnologies: ['nodejs', 'reactjs','html5', 'css3', 'vite', 'webpack', 'firebase', 'heroku', 'eslint', 'prettier', 'git', 'mongodb']
+      ]
     };
   },
   methods: {
@@ -51,137 +49,57 @@ export default {
 </script>
 
 <template>
-  <VTabPanelCard>
-    <div class="experience-list">
-      <div
-        v-for="experience in experiences"
-        :key="experience.id"
-        class="experience-list-item"
-      >
-        <div class="flex flex-row mt-3 p-1">
-          <div class="hidden md:block basis-auto mr-2 mt-1 list-bullet">
-            <img
-              :src="experience.companyLogo"
-              alt="company-logo"
-              class="company-logo"
-              @click="openLink(experience.companyLink)"
-            >
+  <div class="pb-4 px-2 md:px-8">
+    <h1 class="text-4xl font-bold text-base-content mb-4">Experience</h1>
 
-            <div class="list-bullet-line">
-              <div class="list-bullet-line-content"/>
+    <div v-for="(experience, index) in experiences"
+          :key="experience.id"
+          class="experience-item">
+      <div class="flex flex-col gap-4">
+        <!-- Header -->
+        <div class="flex items-center gap-4">
+          <div class="avatar">
+            <div class="w-16 h-16 rounded bg-base-300">
+              <img
+                :src="experience.companyLogo"
+                :alt="experience.company"
+                class="cursor-pointer"
+                @click="openLink(experience.companyLink)"
+              />
             </div>
           </div>
 
-          <div class="basis-auto text-left">
-            <div class="flex flex-row">
-              <div class="basis-auto md:hidden mr-2 mt-1">
-                <img
-                  :src="experience.companyLogo"
-                  alt="company-logo"
-                  class="company-logo"
-                  @click="openLink(experience.companyLink)"
-                >
-              </div>
+          <div>
+            <h2 class="text-2xl text-base-content">
+              {{ experience.role }}
+            </h2>
+            <h3
+              class="text-lg text-primary hover:underline cursor-pointer"
+              @click="openLink(experience.companyLink)"
+            >
+              {{ experience.company }}
+            </h3>
+          </div>
+        </div>
 
-              <div class="basis-auto">
-                <h2
-                  class="text-xl font-bold title-color"
-                  @click="openLink(experience.companyLink)"
-                >
-                  {{ experience.company }}
-                </h2>
-                <div class="font-bold">{{ experience.role }}</div>
-              </div>
-            </div>
+        <!-- Description -->
+        <p class="text-base-content/70">
+          {{ experience.description }}
+        </p>
 
-            <div class="flex py-2">
-              <div class="basis-auto">
-                {{ experience.description }}
-              </div>
-            </div>
-            
-            <div class="flex flex-row align-center pt-2">
-              <div
-                v-for="techonlogy in experience.technologies"
-                :key="techonlogy"
-                class="basis-auto pr-2"
-              >
-                <VIcon :icon="techonlogy" height="24px"/>
-              </div>
-            </div>
+        <!-- Technologies -->
+        <div class="flex flex-wrap gap-2">
+          <div
+            v-for="technology in experience.technologies"
+            :key="technology"
+            class="badge p-4 text-base-content/80 bg-base-200 hover:bg-base-300 cursor-default"
+          >
+            {{ technology }}
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="divider">o</div>
-
-    <div class="flex flex-row justify-center">
-    <div class="basis-auto text-center">
-      <h2 class="text-xl font-bold title-color">Other Technologies I Use</h2>
-
-      <div class="flex flex-row flex-wrap md:flex-nowrap align-center mt-2">
-        <div
-          v-for="techonlogy in otherTechnologies"
-          :key="techonlogy"
-          class="basis-1/6 md:basis-auto p-2"
-        >
-          <VIcon :icon="techonlogy" height="36px"/>
-        </div>
-      </div>   
+      <div v-if="index !== experiences.length - 1" class="divider"/>
     </div>
-    </div>
-  </VTabPanelCard>
+  </div>
 </template>
-
-<style lang="scss">
-  .experience-list {
-    &-item {
-      .company-logo {
-        border-radius: 8px;
-        height: 48px;
-        width: 48px;
-        cursor: pointer;
-      }
-
-      h2 {
-        cursor: pointer;
-        color: hsl(var(--ac));
-
-        &:hover {
-          text-decoration: underline;
-        }
-      }
-
-      .list-bullet {
-        img {
-          max-width: inherit;
-        }
-
-        &-line {
-          height: 100%;
-          width: 100%;
-          display: flex;
-          justify-content: center;
-
-          &-content {
-            $content-color: hsl(var(--bc));
-
-            width: 4px;
-            background-color: $content-color;
-            border: 1px solid $content-color;
-            height: 100%;
-          }
-        }
-      }
-
-      &:last-child {
-        .list-bullet .list-bullet-line .list-bullet-line-content {
-          width: 0;
-          border: none;
-          height: 100%;
-        }
-      }
-    }
-  }
-</style>
