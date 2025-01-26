01d2dd7c0c2eed6ce896b1a80c1fe01b3aa6ad21
ssamilg <samil-98@hotmail.com>
2025-01-27 00:44:50 +0300

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -103,9 +103,8 @@ const projects = [
     description: 'Iftar Timer is a simple and easy-to-use application that helps you to calculate the time of Iftar.',
     image: new URL('@/assets/images/iftar-timer.png', import.meta.url),
     technologies: ['Vue.js', 'Tailwind'],
-    liveUrl: '#',
-    sourceUrl: '#',
-    featured: true
+    liveUrl: 'https://iftartime.netlify.app/',
+    sourceUrl: 'https://github.com/ssamilg/iftar-time-app',
   },
   {
     id: 'weather-forecast-demo',
@@ -113,8 +112,8 @@ const projects = [
     description: 'Weather Forecast Demo is a simple and easy-to-use application that shows the weather forecast of the selected city.',
     image: new URL('@/assets/images/weather-forecast.png', import.meta.url),
     technologies: ['Vue', 'Vuetify'],
-    liveUrl: '#',
-    sourceUrl: '#'
+    liveUrl: 'https://ssg-weather-app.netlify.app/',
+    sourceUrl: 'https://github.com/ssamilg/weather-forecast-app'
   },
   {
     id: 'meme-generator-demo',
@@ -122,8 +121,8 @@ const projects = [
     description: 'Meme Generator Demo is a simple and easy-to-use application that helps you to generate memes.',
     image: new URL('@/assets/images/meme-generator.png', import.meta.url),
     technologies: ['Next.js'],
-    liveUrl: '#',
-    sourceUrl: '#'
+    liveUrl: 'https://meme-generator-ssamilg.vercel.app/',
+    sourceUrl: 'https://github.com/ssamilg/meme-generator'
   }
 ]
 
@@ -737,6 +736,7 @@ const skillsClasses = computed(() => ({
                     <a
                       :href="projects[0].liveUrl"
                       class="text-white/70 hover:text-primary transition-all duration-300 flex items-center gap-2 group-hover:translate-x-2"
+                      target="_blank"
                     >
                       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
@@ -747,6 +747,7 @@ const skillsClasses = computed(() => ({
                     <a
                       :href="projects[0].sourceUrl"
                       class="text-white/70 hover:text-primary transition-all duration-300 flex items-center gap-2 group-hover:translate-x-2"
+                      target="_blank"
                     >
                       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" fill="currentColor">
                         <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
@@ -800,7 +801,8 @@ const skillsClasses = computed(() => ({
                   <div class="flex gap-4">
                     <a
                       :href="project.liveUrl"
-                      class="text-white/70 hover:text-primary transition-all duration-300 flex items-center gap-2 text-sm group-hover:translate-x-2"
+                      class="text-white/70 hover:text-primary transition-all duration-300 flex items-center gap-2 text-sm group-hover:translate-x-2 cursor-pointer"
+                      target="_blank"
                     >
                       <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
@@ -810,7 +812,8 @@ const skillsClasses = computed(() => ({
                     </a>
                     <a
                       :href="project.sourceUrl"
-                      class="text-white/70 hover:text-primary transition-all duration-300 flex items-center gap-2 text-sm group-hover:translate-x-2"
+                      class="text-white/70 hover:text-primary transition-all duration-300 flex items-center gap-2 text-sm group-hover:translate-x-2 cursor-pointer"
+                      target="_blank"
                     >
                       <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" fill="currentColor">
                         <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
